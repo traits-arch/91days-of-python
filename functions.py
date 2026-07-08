e014fbc33aabce6aeeef94117a9c9f97c92bc2ed
traits-arch <brothertraits@gmail.com>
2026-07-08 20:23:15 +0530

    recursion

--- functions.py
+++ functions.py
@@ -9,3 +9,30 @@ w= int(input("Give us your marks in eng: "))
 e= int(input("Give us your marks in pbi: "))
 
 cal_avg(q,w,e)
+
+cities= ['karachi','lahore','islamabad','quetta','peshawar']
+
+def print_length(cities):
+    for items in cities:
+        print(items, end=" ")
+print_length(cities)
+    
+
+# odd even checker
+def odd_even(num):
+    if num%2==0:
+        print(num,"is even")
+    else:
+        print(num,"is odd")
+
+num = int(input("Enter a number to check if its odd or even: "))
+odd_even(num)
+
+#recursion
+def show(n):
+    if n==0:
+        return 
+    print(n)
+    show(n-1)
+
+show(12)
