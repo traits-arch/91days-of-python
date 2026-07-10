d247e71013b6b4822d0619bff4abfe09335a3285
traits-arch <brothertraits@gmail.com>
2026-07-10 21:16:06 +0530

    demotext for file practice

---
+++ files.py
@@ -0,0 +1,8 @@
+open("demotext.txt", "a").write("\nHave a good day btw")
+f = open("demotext.txt", "r")
+data = f.read()
+line1 = f.readline(7)
+print(data)
+print(type(data))
+print(line1)
+f.close()
