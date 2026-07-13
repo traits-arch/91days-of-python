fb8cae23076d20c9f13306923bd94b145c5f5bba
traits-arch <brothertraits@gmail.com>
2026-07-13 21:05:20 +0530

    replacing content in i/o

--- files.py
+++ files.py
@@ -13,4 +13,20 @@ with open("demotext.txt", "r") as f:
     f.close()
 with open("demotext.txt", "a") as f:
     f.write("\n And dont forget to update github")
+    print(data)
     f.close()
+
+with open("demotext.txt", "r") as f:
+    data= f.read()
+    data= data.replace("Have a good day btw", "Have a great day")
+    print(data)
+    f.close()
+
+with open("demotext.txt", "r+") as f:
+    data= f.read()
+    if data.find("good day") != -1:
+       print("Found")
+    else:
+         print("Not Found")
+    f.close()
+    
