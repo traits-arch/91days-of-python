5c9c7dcbe504496fb9163a5722dec8fa47d77c7f
traits-arch <brothertraits@gmail.com>
2026-07-17 21:26:32 +0530

    a module to save functions

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
 def greet(name):
-    print("Hello ",name,"Have a good day")
-name = str(input("Whats your name?"))
+    print("Hello",name,",Have a good day")
+name = str(input("Whats your name?: "))
 greet(name)
