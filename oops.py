2b2e6982019e8db7784872117044c5d1d498c5d4
traits-arch <brothertraits@gmail.com>
2026-07-20 23:27:36 +0530

    usage of __innit__

--- oops.py
+++ oops.py
@@ -1,7 +1,20 @@
 #class
 class student:
+      def __init__(self):
+            print(self)
       name= "Omar Pal"
 
 #object
 s1= student()
 print(s1)
+
+class std: 
+      def __init__(self, name, marks, year):
+            self.name = name 
+            self.marks = marks 
+            self.year = year 
+
+std1= std("karan",88,2025)
+print("Name is: ", std1.name, "\nMarks: ", std1.marks, "\nYear: ",std1.year)    
+std2= std("raj",92,2025)
+print("Name is: ", std2.name, "\nMarks: ", std2.marks, "\nYear: ",std2.year)          
