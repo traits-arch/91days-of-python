878a8d55884cad0af90f75251a1b5b570aff1790
traits-arch <brothertraits@gmail.com>
2026-07-20 23:52:25 +0530

    qr code maker

--- modules.py
+++ modules.py
@@ -3,3 +3,10 @@ def greet(name):
 name = str(input("Whats your name?: "))
 greet(name)
  
+import qrcode
+data = input("Enter the data you wish to be converted into QR: ")
+def qr_maker():
+    img = qrcode.make(data)
+    img.save('c:\Users\broth\OneDrive\Desktop\python\91days-of-python\qrcode.png')
+
+qr_maker(data)
