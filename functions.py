c636cc45317b66d13833d456defeed23f613e326
traits-arch <brothertraits@gmail.com>
2026-07-23 23:55:35 +0530

    recursion practice

--- functions.py
+++ functions.py
@@ -40,6 +40,15 @@ def show(n):
     print(n)
     show(n-1)
 
+def sum_list(numbers):
+  if len(numbers) == 0:
+    return 0
+  else:
+    return numbers[0] + sum_list(numbers[1:])
+
+my_list = [1, 2, 3, 4, 5]
+print(sum_list(my_list)) 
+
 show(12)
 time.sleep(1)
 # currency converter 
