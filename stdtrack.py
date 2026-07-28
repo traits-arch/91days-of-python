b892e1393989b2868e1d05e5e7d4c0e040263693
traits-arch <brothertraits@gmail.com>
2026-07-28 19:51:04 +0530

    check avg and find students

--- stdtrack.py
+++ stdtrack.py
@@ -79,3 +79,68 @@ def std_add(Name):
     # SAVE
     save_students()
     print(f"\nSuccessfully saved {Name} to permanent storage!")
+
+def std_avg(Student):
+        if Student in stds:
+                marks = stds[Student]["Marks"]
+                total = 0
+                count = 0
+                for subject, score in marks.items():
+                        if score != "N/A":
+                         total += score
+                        count += 1
+                if count > 0:
+                        avg = total / count
+                        print(f"The average marks for {Student} is: {avg:.2f}")
+                        stds[Student]["Average"]= avg
+                else:
+                        print("No valid numerical marks found.")
+        else:
+                print("Student not found.")
+def dash():
+    time.sleep(0.5)
+    print("\n-----Welcome to Student Dashboard-----")
+    time.sleep(0.5)
+    print("Choose one of the options: ")
+    print(
+        "1. Add a Student\n2. Check Average\n3. Find a student\n4. Show All\n5. Find the topper\n6. Exit"
+    )
+
+    try:
+        option = int(input("Choose one(1-6): "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        return
+
+    if option == 1:
+        name = input("Enter the name of the student: ")
+        std_add(name)
+    elif option == 2:
+        Student = str(input("Name of the student: "))
+        std_avg(Student)
+    elif option == 3:
+        Student = str(input("Name of the student: "))
+        if Student in stds:
+            print(f"Student {Student} is found!")
+            print(json.dumps(stds[Student], indent=2))
+        else:
+            print("Student not found.")
+    elif option == 4:
+        print("\n--- All Students ---")
+        print(json.dumps(stds, indent=2))
+    elif option == 5:
+        stds["Average"]
+    elif option == 6:
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid input, Please choose between (1-6)")
+
+
+# Loop to continuously run the dashboard until you want to stop
+while True:
+    dash()
+    sarg = input("\n(a) Repeat or (b) Exit: ").lower()
+    if sarg != "a":
+        print("Exiting Program. All data safely saved!")
+        break
